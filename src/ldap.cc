95ccf74d1d287866ade775877d6bdcc8cde42129
Eduard Čuba <ecuba@redhat.com>
2017-10-13 20:41:39 +0200

    parse bindRequest header

--- src/ldap.cc
+++ src/ldap.cc
@@ -11,16 +11,28 @@ class ldapContext
     int client = 0;
     int level = 0;
     int length1 = 0;
+    int length2 = 0;
     int received1 = 0;
+    int received2 = 0;
     int msgId = 0;
+    int protocol = 0;
 };
+
+unsigned char *
+processLength (ldapContext *context);
 
 static unsigned char
 getByte (ldapContext *context)
 {
-    if (context->level == 1) {
-        context->received1 += 1;
+    switch (context->level) {
+        case 1:
+            context->received1 += 1;
+            break;
+        case 2:
+            context->received2 += 1;
+            break;
     }
+
     return receiveByte (context->client);
 }
 
@@ -38,30 +50,129 @@ ldapError (ldapContext *context, int type)
         case ERR_NOT_IMPLEMENTED:
             printE ("Feature not implemented");
             break;
+        case ERR_UNKNOWN_PROTOCOL:
+            break;
     }
     return NULL;
 }
 
 static unsigned char *
+generateResponse (ldapContext *context)
+{
+    return NULL; // TODO
+}
+
+static unsigned char *
+processMessageEnd (ldapContext *context)
+{
+    unsigned char data = 0; // FIXME support 0xA0
+
+    if (data == 0 || data == MSG_END) {
+        printD ("Message correct");
+        return generateResponse (context);
+    }
+    printE ("Message corrupt!");
+    return ldapError (context, ERR_MSG);
+}
+
+static unsigned char *
+processBindRequestAuth (ldapContext *context)
+{
+    unsigned char data = getByte (context);
+
+    if (data != MSG_BIND_REQUEST_AUTH) {
+        pErrHex ("Invalid bindRequest auth, should start with 0x80", data);
+        return ldapError (context, ERR_BIND_REQUEST);
+    }
+
+    // simple auth length
+    unsigned char len = getByte (context);
+
+    char *name = NULL;
+
+    if (len) {
+        return ldapError (context, ERR_NOT_IMPLEMENTED);
+    }
+
+    return processMessageEnd (context);
+}
+
+static unsigned char *
+processBindRequestName (ldapContext *context)
+{
+    unsigned char data = getByte (context);
+
+    if (data != MSG_BIND_REQUEST_NAME) {
+        pErrHex ("Invalid bindRequest name, should start with 0x04", data);
+        return ldapError (context, ERR_BIND_REQUEST);
+    }
+
+    // name length
+    unsigned char len = getByte (context);
+
+    if (len) {
+        return ldapError (context, ERR_NOT_IMPLEMENTED);
+    }
+
+    return processBindRequestAuth (context);
+}
+
+static unsigned char *
+processBindRequest (ldapContext *context)
+{
+    printD ("Protocol: bindRequest");
+
+    unsigned char data = getByte (context);
+
+    if (data != MSG_ID) {
+        pErrHex ("Invalid bindRequest sequence, should be 0x02", data);
+        return ldapError (context, ERR_BIND_REQUEST);
+    }
+
+    data = getByte (context);
+
+    if (data != 0x01) {
+        pErrHex ("Invalid bindRequest sequence, should be 0x01", data);
+        return ldapError (context, ERR_BIND_REQUEST);
+    }
+
+    data = getByte (context);
+
+    if (data < 1 || data > 127) {
+        pErrHex ("Invalid bindRequest version, should be in <1, 127>", data);
+        return ldapError (context, ERR_BIND_REQUEST);
+    }
+
+    return processBindRequestName (context);
+}
+
+static unsigned char *
 processProtocolOp (ldapContext *context)
 {
-    while (context->received1 < context->length1) {
-        getByte (context);
+    unsigned char protocolOp = getByte (context);
+
+    switch (protocolOp) {
+        case MSG_BIND_REQUEST:
+        case MSG_SEARCH_REQUEST:
+            context->protocol = protocolOp;
+            return processLength (context);
     }
-    return NULL;
+
+    pErrHex ("Unknown protocol", protocolOp);
+    return ldapError (context, ERR_UNKNOWN_PROTOCOL);
 }
 
 static unsigned char *
 processLdapMessage (ldapContext *context)
 {
     // 0x02
-    unsigned char data1 = receiveByte (context->client);
+    unsigned char data1 = getByte (context);
 
     // FIXME not sure what this is <0x01, 0x04>
-    unsigned char data2 = receiveByte (context->client);
+    unsigned char data2 = getByte (context);
 
     // message ID FIXME this should be in <0, 2^32-1>
-    context->msgId = receiveByte (context->client);
+    context->msgId = getByte (context);
 
     if (data1 != MSG_ID) {
         pErrHex ("Invalid message, should be 0x02", data1);
@@ -73,15 +184,14 @@ processLdapMessage (ldapContext *context)
         return ldapError (context, ERR_MSG);
     }
 
-    printD("Message ID: " << context->msgId);
+    printD ("Message ID: " << context->msgId);
     return processProtocolOp (context);
 }
 
-static unsigned char *
+unsigned char *
 processLength (ldapContext *context)
 {
     context->level += 1;
-
     unsigned char len = getByte (context);
 
     if (len == 0) {
@@ -89,16 +199,21 @@ processLength (ldapContext *context)
         return ldapError (context, ERR_LENGTH);
     }
 
-    // get length of the message
-    if (context->level == 1) {
-        context->length1 = len;
-        printD("Level 1 length: " << dec << context->length1);
-
-    } else {
-        return ldapError (context, ERR_NOT_IMPLEMENTED);
+    switch (context->level) {
+        case 1:
+            context->length1 = len;
+            printD ("Level 1 length: " << dec << context->length1);
+            return processLdapMessage (context);
+        case 2:
+            context->length2 = len;
+            printD ("Level 2 length: " << dec << context->length2);
+            switch (context->protocol) {
+                case MSG_BIND_REQUEST:
+                    return processBindRequest (context);
+            }
+            break;
     }
-
-    return processLdapMessage (context);
+    return ldapError (context, ERR_NOT_IMPLEMENTED);
 }
 
 /**
--- src/ldap.h
+++ src/ldap.h
@@ -5,12 +5,23 @@
 #define ERR_HEAD 0
 #define ERR_LENGTH 1
 #define ERR_MSG 2
+#define ERR_UNKNOWN_PROTOCOL 3
+#define ERR_BIND_REQUEST 4
 
 #define ERR_NOT_IMPLEMENTED -1
 
 
 #define MSG_LDAP 0x30
 #define MSG_ID 0x02
+#define MSG_END 0xA0
+
+#define MSG_BIND_REQUEST 0x60
+#define MSG_BIND_REQUEST_NAME 0x04
+#define MSG_BIND_REQUEST_AUTH 0x80
+
+
+
+#define MSG_SEARCH_REQUEST 0x63
 
 unsigned char *
 processMessage (int client);
